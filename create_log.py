68e40f46b9692593b97634b9c994ce7bcf9b0bb4
javo2002 <javi22.college@gmail.com>
2024-08-03 16:09:56 -0400

    Create create_log.py

---
+++ create_log.py
@@ -0,0 +1,9 @@
+# Path to the log file
+log_file_path = 'network_traffic.log'
+
+# Clear the log file
+with open(log_file_path, 'w') as file:
+    file.write('')
+
+# Optional: Add a timestamp entry indicating the log was cleared
+
